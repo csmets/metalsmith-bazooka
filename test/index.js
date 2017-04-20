20ec34f5a602198f793112e9b98bb98532707755
csmets <clyde.smets@gmail.com>
2017-04-21 09:26:36 +1000

    Updated

--- test/index.js
+++ test/index.js
@@ -1,3 +1,31 @@
 const test = require('ava');
-const metalsmith = require('metalsmith');
-const bazooka = require('..');
+const Metalsmith = require('metalsmith');
+const bazooka = require('../');
+
+/*
+test('Testing if metalsmith-bazooka loads', (t) => {
+    const testPath = 'text/fixtures/';
+
+    new Metalsmith(testPath)
+        .clean(true)
+        .use(bazooka())
+        .build(() => {
+            t.pass();
+        });
+});
+*/
+test('Compiling of bazooka data', (t) => {
+    const testPath = 'test/fixtures';
+    new Metalsmith(testPath)
+        .source('src')
+        .destination('build')
+        .use(bazooka())
+        .build((err) => {
+            if (err) {
+                t.fail();
+                throw err;
+            } else {
+                t.pass();
+            }
+        });
+});
